7008c95bae47657b1d1c4524ac94943209e2cd68
lixian-alt <2356933851@qq.com>
2025-08-20 11:20:09 +0800

    tag样式编写和高亮效果显示以及功能实现

--- src/store/menu.js
+++ src/store/menu.js
@@ -1,11 +1,24 @@
 const state = {
     isCollapse: false,// 菜单是否折叠
-    selectMenu:[]
+    selectMenu: []
 
 }
 const mutations = {
     collapseMenu(state) {
         state.isCollapse = !state.isCollapse
+    },
+    addMenu(state, payload) {
+        //对数据进行去重
+        const index = state.selectMenu.findIndex(item => item.path === payload.path)
+        if (index === -1) {
+            state.selectMenu.push(payload)
+        }
+    },
+    closeMenu(state, payload) {
+        //找到点击数据的索引
+        const index = state.selectMenu.findIndex(val => val.name === payload.name);
+        //通过索引删除指定元素
+        state.selectMenu.splice(index, 1);
     }
 }
 
